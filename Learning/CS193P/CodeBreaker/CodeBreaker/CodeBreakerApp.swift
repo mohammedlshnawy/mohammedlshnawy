59a7e90814fe86201eeb76cd25ca746ad9f6c387
Mohammed Shnawy <mohammedlshnawy@gmail.com>
2026-01-15 13:53:46 +0200

    Initialize CodeBreaker SwiftUI project
    Set up the initial CodeBreaker app structure with SwiftUI, including project files, workspace, asset catalogs, and basic ContentView displaying a globe image and text. This commit establishes the foundation for further development.

---
+++ Learning/CS193P/CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Mohammed Shnawy on 15/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Learning/CS193P/CodeBreaker/CodeBreaker/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by Mohammed Shnawy on 15/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing:10) {
+                Image(systemName: "globe")
+                Text("Hello")
+                Text("CS193P")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
